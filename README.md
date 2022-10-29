57e1d9f076b9008509fd81758fc850ce5362d159
Victor Okpon <vikzy2rhyme@yahoo.com>
2022-10-29 11:18:44 +0100

    Add routing

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MERN stack Ecommerce web app
+# ECOM - MERN stack Ecommerce web app
 
 # Steps
 
@@ -6,3 +6,12 @@
 2. Install tools
 3. Create react App
 4. Set up Git repository
+5. List Products
+   1. create product array
+   2. add product images
+   3. render products
+   4. style products
+6. Add routing
+   1. install react-router-dom
+   2. create route for home screen
+   3. create router for product screen
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
-// import logo from './logo.svg';
-// import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Ecom website</a>
-      </header>
-      <main>List products</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">Ecom website</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:slug" element={<ProductScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
